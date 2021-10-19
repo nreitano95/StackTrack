aa1e145087a83d498dcc7776451a88bbd0c20738
Richard Nguyen <nguyen.richard.v@gmail.com>
2021-10-19 07:27:56 -0700

    add urls and barebones views for jobs and internships

--- opportunities/urls.py
+++ opportunities/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='opportunities-home'),
-    path('about/', views.about, name='opportunities-about'),    
-
+    path("", views.home, name="opportunities-home"),
+    path("about/", views.about, name="opportunities-about"),
+    path("jobs/", views.jobs, name="opportunities-jobs"),
+    path("internships/", views.internships, name="opportunities-internships"),
 ]
-
