fc0c54b0642407e82eaffed07d3b242077ef9c7b
Richard Nguyen <nguyen.richard.v@gmail.com>
2021-10-21 12:42:42 -0700

    fix syntax error from merge

--- opportunities/urls.py
+++ opportunities/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.home, name="opportunities-home"),
     path("about/", views.about, name="opportunities-about"),
     path("jobs/", views.jobs, name="opportunities-jobs"),
-    path("jobs/delete/<str:job_id>/", views.delete_job, name="opportunities-delete-job")
+    path("jobs/delete/<str:job_id>/", views.delete_job, name="opportunities-delete-job"),
     path("skills/", views.user_skills, name="user-skills"),
 ]
