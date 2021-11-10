eb90b9a116cbcd3453575624a522768309032b69
Richard Nguyen <nguyen.richard.v@gmail.com>
2021-11-09 17:48:14 -0800

    hide user field from addjob form

--- opportunities/forms.py
+++ opportunities/forms.py
@@ -11,7 +11,6 @@ class AddJobForm(ModelForm):
     class Meta:
         # specify model to be used
         model = Job
-        
         # specify fields to be used
         fields = [
             "author",
@@ -23,6 +22,8 @@ class AddJobForm(ModelForm):
             "application_status",
             "skills"
         ]
+        # widgets
+        widgets = {"author": forms.HiddenInput()}
     skills = forms.ModelMultipleChoiceField(
         queryset=Skills.objects.all(),
         widget=forms.CheckboxSelectMultiple
